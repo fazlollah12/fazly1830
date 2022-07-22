cf96b5427844b7a9827c6fdc0d031a4378788df6
fazly <fazlollah.khodaddae@stud.umfcd.ro>
2022-07-22 14:08:35 +0300

    11111111

---
+++ sss.py
@@ -0,0 +1,66 @@
+num1=float(input("enter note 1"))
+if(num1)  > 10 :
+    print("data is not valid")
+else :  
+     
+    if (num1)<0:
+        print("data isnot valid")
+    else :
+        if(num1)>=5 :
+            print("pass")
+
+        
+
+
+
+
+
+
+num2=float(input ("enter note 2 "))
+if(num2)  > 10 :
+    print("data is not valid")
+else :  
+     
+    if (num2)<0:
+        print("data isnot valid")
+    else :
+        if(num2)>=5 :
+            print("pass")
+num3=float(input ("enter note 3"))
+if(num3)  > 10 :
+    print("data is not valid")
+else :  
+     
+    if (num3)<0:
+        print("data isnot valid")
+    else :
+        if(num3)>=5 :
+            print("pass")
+num4=float(input ("enter note 4"))
+if(num4)  > 10 :
+    print("data is not valid")
+else :  
+     
+    if (num4)<0:
+        print("data isnot valid")
+    else :
+        if(num4)>=5 :
+            print("pass")
+num5=float(input ("enter note 5"))
+if(num5)  > 10 :
+    print("data is not valid")
+else :  
+     
+    if (num5)<0:
+        print("data isnot valid")
+    else :
+        if(num5)>=5 :
+            print("pass")
+average=float(input(average=((num1) + (num2) +(num3) +(num4) +(num5)) /5))
+if (average) >7:
+    print("pass")
+else  :
+    print("failed")
+    
+
+
